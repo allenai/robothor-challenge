576379afae5e9b01bdcc0cf050d2142a96accf55
Eric Kolve <erick@allenai.org>
2020-02-11 11:02:12 -0800

    adding error message if nvidia card not found

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-NVIDIA_VERSION=`cat /proc/driver/nvidia/version` docker build -t robothor-challenge:latest .
+if [ -e /proc/driver/nvidia/version ]; then
+    NVIDIA_VERSION=`cat /proc/driver/nvidia/version` docker build -t robothor-challenge:latest .
+else
+    echo "Error: Nvidia driver not found at /proc/driver/nvidia/version; Please ensure you have an Nvidia GPU device and appropriate drivers are installed."
+    exit 1;
+fi;
+
+
