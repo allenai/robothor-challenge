c978f90034fe02f0a9f00077566eb2d032bf237e
Eric Kolve <erick@allenai.org>
2020-02-21 15:47:19 -0800

    adding check for display env variable before using local X11

--- scripts/evaluate_train.sh
+++ scripts/evaluate_train.sh
@@ -6,7 +6,7 @@ export ROBOTHOR_BASE_DIR=`pwd`
 
 # Inference on train split
 X11_PARAMS=""
-if [ -e /tmp/.X11-unix ]; then
+if [[ -e /tmp/.X11-unix && ! -z ${DISPLAY+x} ]]; then
   echo "Using local X11 server"
   X11_PARAMS="-e DISPLAY=$DISPLAY  -v /tmp/.X11-unix:/tmp/.X11-unix:rw"
   xhost +local:root
@@ -14,7 +14,7 @@ fi;
 
 docker run  -v $ROBOTHOR_BASE_DIR/dataset:/opt/robothor-challenge/dataset -e "CHALLENGE_SPLIT=train" -e "CHALLENGE_CONFIG=/opt/robothor-challenge/dataset/challenge_config.yaml" --privileged $X11_PARAMS -it robothor-challenge:latest python3 example_agent.py
 
-if [ -e /tmp/.X11-unix ]; then
+if [[ -e /tmp/.X11-unix && ! -z ${DISPLAY+x} ]]; then
     xhost -local:root
 fi;
 
